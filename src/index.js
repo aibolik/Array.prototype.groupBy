e4ebf0d0cfa4f7541bb3814417065c8db90f4860
Aibol Kussain <Aibol_Kussain@epam.com>
2017-12-12 12:07:41 +0300

    wip: implemented solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function groupBy(function) {
-    // your solution here
+module.exports = function groupBy(f = val => val) {
+    var result = {};
+    for (var val of this) {
+      if (result[f(val)]) {
+        result[f(val)].push(val);
+      } else {
+        result[f(val)] = [val];
+      }
+    }
+    return result;
 }
+
+// console.log([1,2,3,2,4,1,5,1,6].groupBy(val => val % 3));
+// console.log([1,2,3,2,4,1,5,1,6].groupBy());
